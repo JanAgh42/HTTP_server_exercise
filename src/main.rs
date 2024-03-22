7bcc4b13933c660622da1c5fd0ce07b7e28d03c4
Jan Agh <Jan.Agh@2ring.com>
2024-03-22 09:51:59 +0100

    second stage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Uncomment this block to pass the first stage
 use std::net::{TcpListener, TcpStream};
 
-let RESPONSE_OK = "HTTP/1.1 200 OK\r\n\r\n";
+const RESPONSE_OK = "HTTP/1.1 200 OK\r\n\r\n";
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
